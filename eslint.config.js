459112a3f6a3c785b2136605a244c9d3ea3e6f4c
KLusvarghi <kauaolusvarghi@gmail.com>
2025-07-16 19:39:36 -0300

    chore: add eslit import plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended", "prettier"], rules: { "no-unused-vars": "off" }, parserOptions: { ecmaVersion: "es2022", sourceType: "module" } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js, "simple-import-sort": simpleImportSort, }, extends: ["js/recommended", "prettier"], rules: {
+      "no-unused-vars": "off", "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    }, parserOptions: { ecmaVersion: "es2022", sourceType: "module" }
+  },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
 ]);
