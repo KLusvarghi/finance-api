792681b5afb17084e41ff448ba68ceea87f07f05
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-02 17:31:26 -0300

    fix: add restore userBadRequestResponse

--- src/controllers/_helpers/user.ts
+++ src/controllers/_helpers/user.ts
@@ -4,3 +4,5 @@ export const emailIsAlreadyInUseResponse = () =>
     badRequest('Invalid e-mail. Please provide a valid one.')
 
 export const userNotFoundResponse = () => notFound('User not found.')
+
+export const userBadRequestResponse = () => notFound('Missing param: userId.')
