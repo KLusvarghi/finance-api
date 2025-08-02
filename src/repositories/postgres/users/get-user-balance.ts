df8628068b41ebfbc5ac147d0125206caf020a53
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-01 23:32:17 -0300

    feat: use get_user_balance function in PostgresGetUserBalanceRepository

--- src/repositories/postgres/users/get-user-balance.ts
+++ src/repositories/postgres/users/get-user-balance.ts
@@ -4,17 +4,7 @@ export class PostgresGetUserBalanceRepository{
   async execute(userId: string){
     const result = await PostgresHelper.query(
       `
-        SELECT
-          COALESCE( SUM(amount)   FILTER (WHERE type = 'EARNING')   , 0) AS earnings,
-          COALESCE( SUM(amount)   FILTER (WHERE type = 'EXPENSE')   , 0) AS expenses,
-          COALESCE( SUM(amount)   FILTER (WHERE type = 'INVESTMENT'), 0) AS investments,
-          -- saldo = ganhos – gastos – investimentos
-          COALESCE( SUM(amount)   FILTER (WHERE type = 'EARNING')   , 0)
-          - COALESCE( SUM(amount) FILTER (WHERE type = 'EXPENSE')   , 0)
-          - COALESCE( SUM(amount) FILTER (WHERE type = 'INVESTMENT'), 0)
-            AS balance
-        FROM transactions
-        WHERE user_id = $1;
+        SELECT * from get_user_balance($1);
       `, 
       [userId]
     )
