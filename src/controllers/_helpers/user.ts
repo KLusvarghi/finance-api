d3eaa7f040132d2b1d12fc90922067fa5fba2b45
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-02 17:30:05 -0300

    refactor: remove unnecesary validators function

--- src/controllers/_helpers/user.ts
+++ src/controllers/_helpers/user.ts
@@ -1,17 +1,6 @@
-import isEmail from 'validator/lib/isEmail'
 import { badRequest, notFound } from './http'
-
-export const invalidPasswordResponse = () => badRequest('Invalid password')
 
 export const emailIsAlreadyInUseResponse = () =>
     badRequest('Invalid e-mail. Please provide a valid one.')
 
 export const userNotFoundResponse = () => notFound('User not found.')
-
-export const userBadRequestResponse = () => notFound('Missing param: userId.')
-
-export const checkIfPasswordIsValid = (password: string) =>
-    password.trim().length >= 6
-
-export const checkIfEmailIsValid = (email: string) => isEmail(email)
-
