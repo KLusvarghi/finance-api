d909bb70bcb2f8687341449ffa300e98e3b5fc2b
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-01 23:29:48 -0300

    feat: update execMigrations script to read and exec all sql files

--- src/infra/db/postgres/migrations/exec.ts
+++ src/infra/db/postgres/migrations/exec.ts
@@ -7,12 +7,11 @@ import { fileURLToPath } from 'url'
 // Para que tenhamos acesso ao filename e dirname, temos que fazer esse macete:
 
 // o metodo "fileURLToPath" tira a parte do path que seria por exemplo "file://Users/kaua-lusvarghi/..."
-  // e o path não pode ter esse "file", transformando em um path convencional
+// e o path não pode ter esse "file", transformando em um path convencional
 const __filename = fileURLToPath(import.meta.url)
 
 // e aqui pegamos o "dirname" do filename que extraimos acima
 const __dirname = path.dirname(__filename)
-
 
 const execMigrations = async () => {
     // conectando ao pool
@@ -20,15 +19,24 @@ const execMigrations = async () => {
 
     try {
         // eu preciso especificar o diretorio atual que estou executando e depois o path do aql da migration
-        const filePath = path.join(__dirname, './02-correct-column-name.sql')
-        // pegando todo o escript dentro do arquivo .sql
-        // sendo essencial colocar o encoding para que não gere erros
-        const script = fs.readFileSync(filePath, 'utf-8')
+        // aqui nos pegamos todos os arquivos com extensão ".sql"
+        const files = fs
+            .readdirSync(__dirname)
+            .filter((file) => file.endsWith('.sql'))
 
-        // executando todo o script/query que estpá na migration
-        await client.query(script)
+        // dessa maneira a gente consegue rodar todas as migrations sequencioalmente sem ter que fucar colcoando o nome delas explicitamente
+        for (const file of files) {
+            const filePath = path.join(__dirname, file)
+            // pegando todo o escript dentro do arquivo .sql
+            // sendo essencial colocar o encoding para que não gere erros
+            const script = fs.readFileSync(filePath, 'utf-8')
+            // executando todo o script/query que estpá na migration
+            await client.query(script)
 
-        console.log('MIgrations executed successfully')
+            console.log(`Migration ${file} executed successfully`)
+        }
+
+        console.log('All Migrations ware executed successfully')
     } catch (error) {
         console.error(error)
     } finally {
@@ -38,4 +46,4 @@ const execMigrations = async () => {
 
 execMigrations()
 
-// para rodar, basta: node 
+// para rodar, basta: node
