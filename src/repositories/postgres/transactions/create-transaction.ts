b2f65077e3541489c7598ec1a6ac9c739d240a22
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-03 23:30:30 -0300

    refactor: update TransactionParamsProps to use Prisma.TransactionCreateInput

--- src/repositories/postgres/transactions/create-transaction.ts
+++ src/repositories/postgres/transactions/create-transaction.ts
@@ -1,20 +1,10 @@
-import { EnumLike } from 'zod/v4/core/util.cjs'
 import { prisma } from '../../../../prisma/prisma'
-import { TransctionType } from '@prisma/client'
-
-interface CreateTransactionParamsProps {
-    id: string
-    user_id: string
-    name: string
-    amount: number
-    date: Date
-    type: TransctionType
-}
+import { Prisma } from '@prisma/client'
 
 export class PostgresCreateTransactionRepository {
-    async execute(createTransactionParams: CreateTransactionParamsProps) {
+    async execute(createTransactionParams: Prisma.TransactionCreateInput) {
         return await prisma.transaction.create({
-            data: createTransactionParams
+            data: createTransactionParams,
         })
     }
 }
