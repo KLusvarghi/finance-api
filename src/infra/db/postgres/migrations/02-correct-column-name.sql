9a878f1dd9bb475776f052d67b5e946df9e8ebd6
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-03 21:31:09 -0300

    refactor: remove unnecessary pages and foldes

--- src/infra/db/postgres/migrations/02-correct-column-name.sql
+++
@@ -1,9 +0,0 @@
--- Migration para corrigir o nome da coluna "amout" para "amount"
-ALTER TABLE transactions RENAME COLUMN amout TO amount;
-
--- para ver o nome das colunas da tabela
--- SELECT column_name
--- FROM information_schema.columns
--- WHERE table_schema = 'public'
---   AND table_name   = 'users'
--- ORDER BY ordinal_position;
