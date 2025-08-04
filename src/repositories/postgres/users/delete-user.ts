4f04e0095a4276bebcbc204d9da8d686b3f1e10f
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-03 22:34:35 -0300

    feat: ensure PostgresDeleteUserRepository use Prisma

--- src/repositories/postgres/users/delete-user.ts
+++ src/repositories/postgres/users/delete-user.ts
@@ -2,6 +2,14 @@ import { prisma } from '../../../../prisma/prisma'
 
 export class PostgresDeleteUserRepository {
     async execute(userId: string) {
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+        })
+
+        if (!user) {
+            return null
+        }
+        
         return await prisma.user.delete({
             where: {
                 id: userId,
