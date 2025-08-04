197f3939a30311f070a0a54ffb0544d981e554b0
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-03 22:14:57 -0300

    feat: ensure CreateUserRepository use Prisma

--- src/repositories/postgres/users/create-user.ts
+++ src/repositories/postgres/users/create-user.ts
@@ -1,4 +1,4 @@
-import { PostgresHelper } from '@/infra/db/postgres/helper'
+import { prisma } from '../../../../prisma/prisma'
 
 interface CreateUserParams {
     id: string
@@ -11,18 +11,14 @@ interface CreateUserParams {
 export class PostgresCreateUserRepository {
     // como nomenclatura, podemos usar execute, handle ou até create
     async execute(createUserParams: CreateUserParams) {
-        const result = await PostgresHelper.query(
-            'INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [
-                createUserParams.id,
-                createUserParams.first_name,
-                createUserParams.last_name,
-                createUserParams.email,
-                createUserParams.password,
-            ],
-        )
-
-        // como retorno, ele nos dará uma lista, e o primeiro item será o usuário criado
-        return result[0]
+        return await prisma.user.create({
+            data: {
+                id: createUserParams.id,
+                first_name: createUserParams.first_name,
+                last_name: createUserParams.last_name,
+                email: createUserParams.email,
+                password: createUserParams.password,
+            },
+        })
     }
 }
