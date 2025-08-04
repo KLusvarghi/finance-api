ad2ed62ebfe85484f14384ae3bbc7fb3d5756c5e
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-04 11:25:56 -0300

    feat: add return of DeleteUserRepository with prisma payload

--- src/repositories/postgres/users/delete-user.ts
+++ src/repositories/postgres/users/delete-user.ts
@@ -1,7 +1,8 @@
+import { Prisma } from '@prisma/client'
 import { prisma } from '../../../../prisma/prisma'
 
 export class PostgresDeleteUserRepository {
-    async execute(userId: string) {
+    async execute(userId: string): Promise<Prisma.UserGetPayload<{}> | null> {
         const user = await prisma.user.findUnique({
             where: { id: userId },
         })
@@ -9,7 +10,7 @@ export class PostgresDeleteUserRepository {
         if (!user) {
             return null
         }
-        
+
         return await prisma.user.delete({
             where: {
                 id: userId,
