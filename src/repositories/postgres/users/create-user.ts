9904ff0ac3a0c1051bc4135a66b7ff0855f67aa1
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-04 11:21:43 -0300

    feat: add return of CreateUserRepository with UserGetPayload

--- src/repositories/postgres/users/create-user.ts
+++ src/repositories/postgres/users/create-user.ts
@@ -5,7 +5,9 @@ type CreateUserParams = Omit<Prisma.UserCreateInput, 'id' | 'transactions'>
 
 export class PostgresCreateUserRepository {
     // como nomenclatura, podemos usar execute, handle ou até create
-    async execute(createUserParams: CreateUserParams) {
+    async execute(
+        createUserParams: CreateUserParams,
+    ): Promise<Prisma.UserGetPayload<{}>> {
         return await prisma.user.create({
             data: createUserParams,
         })
