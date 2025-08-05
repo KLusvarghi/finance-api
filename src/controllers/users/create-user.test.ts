6deee5026b6e335ceace7c2d4100ca55e89a60ea
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-05 13:17:04 -0300

    test: add validation tests for CreateUserController to ensure proper error responses

--- src/controllers/users/create-user.test.ts
+++ src/controllers/users/create-user.test.ts
@@ -29,7 +29,7 @@ describe('Create User Controller', () => {
         const createUserController = new CreateUserController(createUserService)
         // como o test precisa criar um user, eu passo para o httpRequest todos os campos que é necessário para criar um user
         const httpRequest = {
-          // todos os parametros serão testados quando passar pelo "createUserController", então elas tem que seguir as regras
+            // todos os parametros serão testados quando passar pelo "createUserController", então elas tem que seguir as regras
             body: {
                 first_name: 'Kauã',
                 last_name: 'Kauã',
@@ -46,9 +46,131 @@ describe('Create User Controller', () => {
         // esperamso que o body exista
         expect(result.body).toBeTruthy()
         // podemos ser bem expecífico no que esperamos
-        expect(result.body?.data?.first_name).toBe("Kauã")
+        expect(result.body?.data?.first_name).toBe('Kauã')
         // quando queremos esperar que algo não seja, usamos o "not"
         expect(result.body).not.toBeUndefined()
         expect(result.body).not.toBeNull()
     })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+        const createUserService = new CreateUserServiceStub()
+        const createUserController = new CreateUserController(createUserService)
+        const httpRequest = {
+            body: {
+                last_name: 'lusvarghi',
+                email: 'kaua@gmail.com',
+                password: '12345678',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body?.message).toBe('First name is required')
+    })
+
+    it('should return 400 if last_name is not provided', async () => {
+        // arrange
+        const createUserService = new CreateUserServiceStub()
+        const createUserController = new CreateUserController(createUserService)
+        const httpRequest = {
+            body: {
+                first_name: 'kaua',
+                email: 'kaua@gmail.com',
+                password: '12345678',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body?.message).toBe('Last name is required')
+    })
+
+    it('should return 400 if Email is not provided', async () => {
+        // arrange
+        const createUserService = new CreateUserServiceStub()
+        const createUserController = new CreateUserController(createUserService)
+        const httpRequest = {
+            body: {
+                first_name: 'kaua',
+                last_name: 'lusvarghi',
+                password: '12345678',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body?.message).toBe('Email is required')
+    })
+
+    it('should return 400 if Email is not valid', async () => {
+        // arrange
+        const createUserService = new CreateUserServiceStub()
+        const createUserController = new CreateUserController(createUserService)
+        const httpRequest = {
+            body: {
+                first_name: 'kaua',
+                last_name: 'lusvarghi',
+                email: 'das',
+                password: '12345678',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body?.message).toBe('Please provide a valid email')
+    })
+
+    it('should return 400 if Password is not provided', async () => {
+        // arrange
+        const createUserService = new CreateUserServiceStub()
+        const createUserController = new CreateUserController(createUserService)
+        const httpRequest = {
+            body: {
+                first_name: 'kaua',
+                last_name: 'lusvarghi',
+                email: 'kaua@gmail.com',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body?.message).toBe('Password is required')
+    })
+    
+    it('should return 400 if Password is not valid', async () => {
+      // arrange
+      const createUserService = new CreateUserServiceStub()
+      const createUserController = new CreateUserController(createUserService)
+      const httpRequest = {
+          body: {
+              first_name: 'kaua',
+              last_name: 'lusvarghi',
+              email: 'kaua@gmail.com',
+              password: '123',
+          },
+      }
+
+      // act
+      const result = await createUserController.execute(httpRequest)
+
+      // assert
+      expect(result.statusCode).toBe(400)
+      expect(result.body?.message).toBe('Password must have at least 6 characters')
+  })
 })
