e89b7befed3a5c4389a027c784ff1eb330fd06e9
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-05 10:40:34 -0300

    chore: setup jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,31 +1,35 @@
-import type { Config } from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
-  // Usa o preset ESM do ts-jest, que já configura o transform pra você
-  preset: 'ts-jest/presets/default-esm',
+    // Usa o preset ESM do ts-jest, que já configura o transform pra você
+    preset: 'ts-jest/presets/default-esm',
 
-  // Node é o ambiente onde seu código roda
-  testEnvironment: 'node',
+    // Node é o ambiente onde seu código roda
+    testEnvironment: 'node',
 
-  // Diz pro Jest: "Trat e arquivos .ts como ESM"
-  extensionsToTreatAsEsm: ['.ts'],
+    // Diz pro Jest: "Trat e arquivos .ts como ESM"
+    extensionsToTreatAsEsm: ['.ts'],
 
-  // Se você tiver aliases @/... no tsconfig, replique aqui:
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
+    // Se você tiver aliases @/... no tsconfig, replique aqui:
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
 
-  globals: {
-    'ts-jest': {
-      // Aponta pro seu tsconfig e ativa o modo ESM
-      tsconfig: 'tsconfig.json',
-      useESM: true
-    }
-  },
-  
+    globals: {
+        'ts-jest': {
+            // Aponta pro seu tsconfig e ativa o modo ESM
+            tsconfig: 'tsconfig.json',
+            useESM: true,
+        },
+    },
 
-  // Onde o Jest deve procurar seus testes
-  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/src/**/*.spec.ts'],
-};
+    // Onde o Jest deve procurar seus testes
+    testMatch: [
+        '<rootDir>/src/**/*.test.ts',
+        '<rootDir>/src/**/*.spec.ts',
+        '<rootDir>/*.test.ts',
+        '<rootDir>/*.spec.ts',
+    ],
+}
 
-export default config;
+export default config
