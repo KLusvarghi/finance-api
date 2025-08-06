d4a8a45a1dd83671bf5391ed1d91629e108d5cc7
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-05 21:37:28 -0300

    test: ensure DeleteUserController returns 404 if user not found

--- src/controllers/users/delete-user.test.ts
+++ src/controllers/users/delete-user.test.ts
@@ -1,9 +1,10 @@
+import { UserRepositoryResponse } from '@/shared'
 import { DeleteUserController } from './delete-user'
 import { faker } from '@faker-js/faker'
 
 describe('DeleteUserController', () => {
     class DeleteUserServiceStub {
-        execute(userId: string) {
+        execute(userId: string): Promise<UserRepositoryResponse | null> {
             return Promise.resolve({
                 id: faker.string.uuid(),
                 first_name: faker.person.firstName(),
@@ -17,7 +18,6 @@ describe('DeleteUserController', () => {
     const httpRequest = {
         params: {
             userId: faker.string.uuid(),
-            
         },
     }
 
@@ -40,13 +40,31 @@ describe('DeleteUserController', () => {
     })
 
     it('should return 400 if userId is invalid', async () => {
-      // arrange
-      const { sut } = makeSut()
+        // arrange
+        const { sut } = makeSut()
 
-      // act
-      const result = await sut.execute({params: {userId: 'invalid-uuid'}})
+        // act
+        const result = await sut.execute({ params: { userId: 'invalid-uuid' } })
 
-      // assert
-      expect(result.statusCode).toBe(400)
-  })
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if user not found', async () => {
+        // arrange
+        const { sut, deleteUserService } = makeSut()
+
+        // mocando para que ele retrone null
+        // jest.spyOn(deleteUserService, 'execute').mockResolvedValue(null)
+        jest.spyOn(deleteUserService, 'execute').mockImplementationOnce(
+            async () => null,
+        )
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(404)
+        // expect(result.body?.message).toEqual("User not found.")
+    })
 })
