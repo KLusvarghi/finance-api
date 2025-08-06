5d3f81e20c53f4fe483315a7c92606d00c3ad95a
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-06 19:06:32 -0300

    test: ensure UpdateUserController return 400 when an passeord is provided

--- src/controllers/users/update-user.test.ts
+++ src/controllers/users/update-user.test.ts
@@ -53,7 +53,7 @@ describe('UpdateUserController', () => {
         expect(result.statusCode).toBe(200)
     })
 
-    it('should return 400 when a n invalid email is provided', async () => {
+    it('should return 400 when an invalid email is provided', async () => {
         // arrange
         const { sut } = makeSut()
 
@@ -70,5 +70,22 @@ describe('UpdateUserController', () => {
         expect(result.statusCode).toBe(400)
     })
 
-    
+    it('should return 400 when an invalid password is provided', async () => {
+      // arrange
+      const { sut } = makeSut()
+
+      // act
+      const result = await sut.execute({
+          ...httpRequest,
+          body: {
+              ...httpRequest.body,
+              password: 'invalid_password',
+          },
+      })
+
+      // asset
+      expect(result.statusCode).toBe(400)
+  })
+
+
 })
