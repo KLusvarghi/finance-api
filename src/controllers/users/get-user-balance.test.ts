d9805c5d4e524985ddfc8718b6a39049fd1e5e32
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-06 11:52:02 -0300

    test: ensure GetUserBalanceController returns 200 when getting user balance

---
+++ src/controllers/users/get-user-balance.test.ts
@@ -0,0 +1,48 @@
+import {
+    // GetUserBalanceParams,
+    GetUserBalanceService,
+    UserBalanceRepositoryResponse,
+} from '@/shared'
+import { faker } from '@faker-js/faker'
+import { GetUserBalanceController } from './get-user-balance'
+import { UserNotFoundError } from '@/errors/user'
+import { Prisma } from '@prisma/client'
+
+describe('GetUserBalanceController', () => {
+    class GetUserBalanceServiceStub implements GetUserBalanceService {
+        execute(
+            // params: GetUserBalanceParams,
+        ): Promise<UserBalanceRepositoryResponse | UserNotFoundError> {
+            return Promise.resolve({
+                earnings: faker.number.float(),
+                expenses: faker.number.float(),
+                investments: faker.number.float(),
+                balance: new Prisma.Decimal(faker.number.float()),
+            })
+        }
+    }
+
+    const makeSut = () => {
+        const getUserBalanceService = new GetUserBalanceServiceStub()
+        const sut = new GetUserBalanceController(getUserBalanceService)
+
+        return { getUserBalanceService, sut }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 when user getting balance', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(200)
+    })
+})
