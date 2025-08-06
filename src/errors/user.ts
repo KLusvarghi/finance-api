a0b7e296fe062bba534ff15f1e3d0b10724bc8df
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-06 12:04:28 -0300

    refactor: improve error handling messages for user and transaction errors

--- src/errors/user.ts
+++ src/errors/user.ts
@@ -1,20 +1,20 @@
 export class EmailAlreadyExistsError extends Error {
     constructor(email: string) {
-      super(`The e-mail ${email} is already in use.`)
-      this.name = 'EmailAlreadyExistsError' // assim a gente deixa mais claro o name do error
+        super(`The e-mail ${email} is already in use.`)
+        this.name = 'EmailAlreadyExistsError' // assim a gente deixa mais claro o name do error
     }
 }
 
 export class UserNotFoundError extends Error {
-  constructor() {
-      super(`User not found`)
-      this.name = 'UserNotFoundError' // assim a gente deixa mais claro o name do error
-  }
+    constructor(userId: string) {
+        super(`User with id ${userId} not found`)
+        this.name = 'UserNotFoundError'
+    }
 }
 
 export class TransactionNotFoundError extends Error {
-  constructor() {
-      super(`Transaction not found`)
-      this.name = 'TransactionNotFoundError' // assim a gente deixa mais claro o name do error
-  }
+    constructor(transactionId: string) {
+        super(`Transaction with id ${transactionId} not found`)
+        this.name = 'TransactionNotFoundError'
+    }
 }
