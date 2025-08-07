9e2662f4856a7bcab83814bf6b9a3bc85673d134
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-07 19:38:10 -0300

    test: ensure return 201 when creating transation successfully

---
+++ src/controllers/transactions/create-transaction.test.ts
@@ -0,0 +1,80 @@
+import {
+    CreateTransactionParams,
+    TransactionRepositoryResponse,
+} from '@/shared'
+import { CreateTransactionController } from './create-transaction'
+import { faker } from '@faker-js/faker'
+import { Prisma } from '@prisma/client'
+
+describe('CreateTransactionController', () => {
+    let sut: CreateTransactionController
+    let createTransactionService: CreateTransactionServiceStub
+    let validTransactionData: any
+
+    class CreateTransactionServiceStub {
+        async execute(
+            _transaction: CreateTransactionParams,
+        ): Promise<TransactionRepositoryResponse> {
+            return Promise.resolve({
+                id: faker.string.uuid(),
+                user_id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                amount: new Prisma.Decimal(Number(faker.finance.amount())),
+                date: faker.date.anytime(),
+                type: faker.helpers.arrayElement([
+                    'EARNING',
+                    'EXPENSE',
+                    'INVESTMENT',
+                ]),
+            })
+        }
+    }
+
+    const makeSut = () => {
+        const createTransactionService = new CreateTransactionServiceStub()
+        const sut = new CreateTransactionController(createTransactionService)
+        return { createTransactionService, sut }
+    }
+
+    beforeEach(() => {
+        // Setup executado antes de cada teste, tendo que especificar o que cada um é, controller e service
+        const { sut: controller, createTransactionService: service } = makeSut()
+        sut = controller
+        createTransactionService = service
+
+        // Dados válidos sempre disponíveis
+        validTransactionData = {
+            user_id: faker.string.uuid(),
+            name: faker.commerce.productName(),
+            // Tendo que definir corretamente o tipo do date, pois o faker.date.anytime() retorna um objeto Date e não uma string
+            date: faker.date.anytime().toISOString(),
+            // type: 'EARNING',
+            type: faker.helpers.arrayElement([
+                'EARNING',
+                'EXPENSE',
+                'INVESTMENT',
+            ]),
+            amount: Number(faker.finance.amount()),
+            // amount: new Prisma.Decimal(
+            //     faker.number.float({ min: 1, max: 1000 }),
+            // ),
+        }
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+        jest.restoreAllMocks()
+    })
+
+    describe('succes cases', () => {
+        it('should return 201 when creating transaction successfully', async () => {
+            // arrange
+            const result = await sut.execute({ body: validTransactionData })
+
+            // assert
+            expect(result.statusCode).toBe(201)
+            expect(result.body?.status).toBe('success')
+            // expect(result.body?.data).toMatchObject(validTransactionData)
+        })
+    })
+})
