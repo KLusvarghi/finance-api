b96f0f6183b1c87319a7199a8784e6582c1c4e47
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-07 19:37:01 -0300

    refactor: update createTransactionParams type to use CreateTransactionParams for consistency

--- src/repositories/postgres/transactions/create-transaction.ts
+++ src/repositories/postgres/transactions/create-transaction.ts
@@ -1,12 +1,12 @@
 import {
-    CreateTransactionParamsProps,
+    CreateTransactionParams,
     TransactionRepositoryResponse,
 } from '@/shared/types'
 import { prisma } from '../../../../prisma/prisma'
 
 export class PostgresCreateTransactionRepository {
     async execute(
-        createTransactionParams: CreateTransactionParamsProps & { id: string },
+        createTransactionParams: CreateTransactionParams & { id: string },
     ): Promise<TransactionRepositoryResponse> {
         return await prisma.transaction.create({
             data: createTransactionParams,
