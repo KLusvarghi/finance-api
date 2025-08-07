e7b22a6bf66544e2d17cc264d3eb91bc904db97c
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-07 00:03:35 -0300

    refactor: enhance userNotFoundResponse to accept a custom message

--- src/controllers/_helpers/user.ts
+++ src/controllers/_helpers/user.ts
@@ -3,6 +3,7 @@ import { badRequest, notFound } from './http'
 export const emailIsAlreadyInUseResponse = () =>
     badRequest('Invalid e-mail. Please provide a valid one.')
 
-export const userNotFoundResponse = () => notFound('User not found.')
+export const userNotFoundResponse = (message?: string) =>
+    notFound(message ? message : 'User not found.')
 
 export const userBadRequestResponse = () => notFound('Missing param: userId.')
