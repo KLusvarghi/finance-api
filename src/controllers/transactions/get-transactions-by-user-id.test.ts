47fb0a519c4ac1dfdc4e99094618a75448634778
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-08 10:27:53 -0300

    test: ensure GetTransactionsByUserIdController returns 200 when find transaction by user id

---
+++ src/controllers/transactions/get-transactions-by-user-id.test.ts
@@ -0,0 +1,58 @@
+import { HttpRequest, TransactionRepositoryResponse } from "@/shared"
+import { GetTransactionsByUserIdController } from "./get-transactions-by-user-id"
+import { faker } from "@faker-js/faker"
+import { Prisma } from "@prisma/client"
+
+describe('GetTransactionsByUserIdController', () => {
+  let sut: GetTransactionsByUserIdController
+  let getTransactionByUserIdService: GetTransactionsByUserIdServiceStub
+  let validUserId: string
+  let validTransactionData: TransactionRepositoryResponse[]
+
+  class GetTransactionsByUserIdServiceStub {
+    execute(userId: string): Promise<TransactionRepositoryResponse[]> {
+      return Promise.resolve(validTransactionData)
+    }
+  }
+
+  const makeSut = () => {
+    const getTransactionByUserIdService = new GetTransactionsByUserIdServiceStub()
+    const sut = new GetTransactionsByUserIdController(getTransactionByUserIdService)
+
+    return { sut, getTransactionByUserIdService }
+  }
+
+  beforeEach(() => {
+    validUserId = faker.string.uuid()
+    validTransactionData = [
+      {
+        id: faker.string.uuid(),
+        user_id: validUserId,
+        name: faker.commerce.productName(),
+        amount: new Prisma.Decimal(Number(faker.finance.amount())),
+        date: faker.date.anytime(),
+        type: 'EARNING',
+      },
+    ]
+
+    const { sut: controller, getTransactionByUserIdService: service } = makeSut() 
+    sut = controller
+    getTransactionByUserIdService = service
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+    jest.restoreAllMocks()
+  })
+
+  describe('success cases', () => {
+    it('should return 200 when finding transactions by user id', async() => {
+      // act
+      const response = await sut.execute({ query: { userId: validUserId } })
+
+      // assert
+      expect(response.statusCode).toBe(200)
+      expect(response.body?.data).toEqual(validTransactionData)
+    })
+  })
+})
