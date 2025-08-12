fe400987dffd6df049e578fa0e12f4cff38445f3
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-11 21:42:01 -0300

    test: add unit tests for PasswordHasherAdapter to validate password hashing

---
+++ src/adapters/password-hasher.test.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+import { PasswordHasherAdapter } from './password-hasher'
+
+describe('PasswordHasherAdapter', () => {
+    it('should return a hash of the password', async () => {
+        // arrange
+        const sut = new PasswordHasherAdapter()
+        const password = faker.internet.password()
+
+        // act
+        const response = await sut.execute(password)
+    
+        // asset
+        expect(response).toBeTruthy()
+        expect(typeof response).toBe('string')
+        expect(response).not.toBe(password)
+      })
+})
