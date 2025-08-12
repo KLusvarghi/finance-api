3587ca64b3c7aa8bc23a086daed7ac2a7d0bc928
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-12 09:10:15 -0300

    refactor: change return type in CreateTransactionRepository to TransactionPublicResponse

--- src/repositories/postgres/transactions/create-transaction.ts
+++ src/repositories/postgres/transactions/create-transaction.ts
@@ -1,13 +1,13 @@
 import {
     CreateTransactionParams,
-    TransactionRepositoryResponse,
+    TransactionPublicResponse,
 } from '@/shared/types'
 import { prisma } from '../../../../prisma/prisma'
 
 export class PostgresCreateTransactionRepository {
     async execute(
         createTransactionParams: CreateTransactionParams & { id: string },
-    ): Promise<TransactionRepositoryResponse> {
+    ): Promise<TransactionPublicResponse> {
         return await prisma.transaction.create({
             data: createTransactionParams,
         })
