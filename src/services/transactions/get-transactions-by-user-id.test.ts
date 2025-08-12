48a017cd06080a0bc28d8eee0a85d0003e96c590
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-12 11:41:53 -0300

    test: add GetTransactionsByUserIdService tests for successful transaction

---
+++ src/services/transactions/get-transactions-by-user-id.test.ts
@@ -0,0 +1,127 @@
+import { faker } from '@faker-js/faker'
+import {
+    TransactionPublicResponse,
+    TransactionRepositoryResponse,
+    UserRepositoryResponse,
+} from '@/shared'
+import { Prisma } from '@prisma/client'
+import { GetTransactionsByUserIdService } from './get-transactions-by-user-id'
+
+describe('DeleteTransactionService', () => {
+    let sut: GetTransactionsByUserIdService
+    let getUserByIdRepository: GetUserByIdRepositoryStub
+    let getTransactionByUserIdRepository: GetTransactionsByUserIdRepositoryStub
+
+    let validGetUserByIdRepositoryResponse: UserRepositoryResponse | null
+    let validTransactionRepositoryResponse:
+        | TransactionRepositoryResponse[]
+        | null
+    let validTransactionServiceResponse: TransactionPublicResponse[] | null
+    let validUserId: string
+
+    class GetUserByIdRepositoryStub {
+        async execute(_userId: string): Promise<UserRepositoryResponse | null> {
+            return Promise.resolve(validGetUserByIdRepositoryResponse)
+        }
+    }
+
+    class GetTransactionsByUserIdRepositoryStub {
+        async execute(
+            _userId: string,
+        ): Promise<TransactionRepositoryResponse[] | null> {
+            return Promise.resolve(validTransactionRepositoryResponse)
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+        const getTransactionByUserIdRepository =
+            new GetTransactionsByUserIdRepositoryStub()
+        const sut = new GetTransactionsByUserIdService(
+            getUserByIdRepository,
+            getTransactionByUserIdRepository,
+        )
+
+        return {
+            sut,
+            getUserByIdRepository,
+            getTransactionByUserIdRepository,
+        }
+    }
+
+    beforeEach(() => {
+        const {
+            sut: service,
+            getUserByIdRepository: getUserByIdRepositoryStub,
+            getTransactionByUserIdRepository:
+                getTransactionByUserIdRepositoryStub,
+        } = makeSut()
+
+        sut = service
+        getUserByIdRepository = getUserByIdRepositoryStub
+        getTransactionByUserIdRepository = getTransactionByUserIdRepositoryStub
+
+        validUserId = faker.string.uuid()
+
+        validGetUserByIdRepositoryResponse = {
+            id: validUserId,
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        }
+
+        validTransactionRepositoryResponse = [
+            {
+                id: faker.string.uuid(),
+                user_id: validUserId,
+                name: faker.lorem.words(3),
+                amount: new Prisma.Decimal(
+                    faker.number.int({ min: 1, max: 1000 }),
+                ),
+                date: faker.date.recent(),
+                type: faker.helpers.arrayElement([
+                    'EARNING',
+                    'EXPENSE',
+                    'INVESTMENT',
+                ]),
+            },
+        ]
+
+        validTransactionServiceResponse = [
+            {
+                id: validTransactionRepositoryResponse[0].id,
+                user_id: validUserId,
+                name: validTransactionRepositoryResponse[0].name,
+                amount: validTransactionRepositoryResponse[0].amount,
+                date: validTransactionRepositoryResponse[0].date,
+                type: validTransactionRepositoryResponse[0].type,
+            },
+        ]
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+        jest.restoreAllMocks()
+        jest.resetAllMocks()
+    })
+
+    describe('error handling', () => {
+        // it('should throw UserNotFoundError if GetUserByIdRepository returns null', async () => {
+        //     // arrange
+        //     jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValueOnce(null)
+        //     // act
+        //     const promise = sut.execute(validUserId)
+        // })
+    })
+
+    describe('success', () => {
+        it('should get transactions by user id successfully', async () => {
+            // act
+            const response = await sut.execute(validUserId)
+
+            // assert
+            expect(response).toEqual(validTransactionServiceResponse)
+        })
+    })
+})
