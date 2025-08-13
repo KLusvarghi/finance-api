fba856b84d4dab7b09827a6d2d85409fa2796402
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-13 18:37:26 -0300

    test: ensure PostgresUpdateUserRepository calls prisma with correct params

--- src/repositories/postgres/users/update-user.test.ts
+++ src/repositories/postgres/users/update-user.test.ts
@@ -1,12 +1,16 @@
 import {
     updateUserRepositoryResponse as fakeUser,
-    updateUserParams,
+    updateUserParams as params,
 } from '@/test'
 import { prisma } from '../../../../prisma/prisma'
 import { PostgresUpdateUserRepository } from './update-user'
 
 describe('PostgresUpdateUserRepository', () => {
     let sut = new PostgresUpdateUserRepository()
+    let updateUserParams = {
+        ...params,
+        password: 'valid_hash',
+    }
 
     describe('success', () => {
         it('should update user on database successfully', async () => {
@@ -16,31 +20,32 @@ describe('PostgresUpdateUserRepository', () => {
             })
             // act
             // tendo que fazer isso pois o password chega ao repository criptografado, e o nosso "updateUserParams" vem com a senha descriptografada
-            const response = await sut.execute(user.id, {
-                ...updateUserParams,
-                password: 'valid_hash',
-            })
+            const response = await sut.execute(user.id, updateUserParams)
 
             // assert
             expect(response).toStrictEqual(fakeUser)
         })
     })
 
-    // describe('validations', () => {
-    //   it('should call Prisma with correct params', async () => {
-    //       // arrange
-    //       // monitoramos o metodo "findUnique" do prisma
-    //       const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+    describe('validations', () => {
+        it('should call Prisma with correct params', async () => {
+            // arrange
+            const user = await prisma.user.create({
+                data: fakeUser,
+            })
+            // monitoramos o metodo "update" do prisma
+            const prismaSpy = jest.spyOn(prisma.user, 'update')
 
-    //       // act
-    //       await sut.execute(fakeUser.email)
+            // act
+            await sut.execute(user.id, updateUserParams)
 
-    //       // assert
-    //       expect(prismaSpy).toHaveBeenCalledWith({
-    //         where: {
-    //             email: fakeUser.email,
-    //         },
-    //       })
-    //     })
-    // })
+            // assert
+            expect(prismaSpy).toHaveBeenCalledWith({
+                where: {
+                    id: user.id,
+                },
+                data: updateUserParams,
+            })
+        })
+    })
 })
