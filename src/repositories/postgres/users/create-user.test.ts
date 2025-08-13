f2ab534efbae05d087924bd8404a81eee412d728
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-13 18:52:58 -0300

    test: ensure PostgresCreateUserRepository throws if Prisma throws

--- src/repositories/postgres/users/create-user.test.ts
+++ src/repositories/postgres/users/create-user.test.ts
@@ -1,28 +1,44 @@
-import { createUserRepositoryResponse as user } from '@/test'
+import {
+    updateUserParams,
+    createUserRepositoryResponse as fakerUser,
+} from '@/test'
 import { PostgresCreateUserRepository } from './create-user'
 import { prisma } from '../../../../prisma/prisma'
 
 describe('PostgresCreateUserRepository', () => {
     let sut = new PostgresCreateUserRepository()
 
+    describe('error handling', () => {
+        it('should throw an error if Prisma throws', async () => {
+            // arrange
+            jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(
+                new Error(),
+            )
+            // act
+            const promise = sut.execute(fakerUser)
+
+            expect(promise).rejects.toThrow(new Error())
+        })
+    })
+
     describe('success', () => {
         it('should create a user on database', async () => {
-            const response = await sut.execute(user)
+            const response = await sut.execute(fakerUser)
 
             expect(response).not.toBeNull()
-            expect(response).toStrictEqual(user)
+            expect(response).toStrictEqual(fakerUser)
         })
     })
 
     describe('validations', () => {
-      it('should call Prisma with correct params', async () => {
-        const prismaSpy = jest.spyOn(prisma.user, 'create')
+        it('should call Prisma with correct params', async () => {
+            const prismaSpy = jest.spyOn(prisma.user, 'create')
 
-        await sut.execute(user)
+            await sut.execute(fakerUser)
 
-        expect(prismaSpy).toHaveBeenCalledWith({
-          data: user,
+            expect(prismaSpy).toHaveBeenCalledWith({
+                data: fakerUser,
+            })
         })
-      })
     })
 })
