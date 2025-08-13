6317db0f8847eac7508665268d5bb31a8b58ee86
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-13 17:50:01 -0300

    test: adjust order of database cleanup in jest setup to ensure proper deletion of related records

--- jest.setup-after-env.ts
+++ jest.setup-after-env.ts
@@ -3,6 +3,7 @@ import { prisma } from './prisma/prisma'
 // antes de cada teste, deletamos todos os usuários e transações, afim de nenhum dado fique no banco e interfira nos testes
 beforeEach(async () => {
   // limpa as tabelas do nosso banco de dados antes de cada teste
-    await prisma.user.deleteMany({})
-    await prisma.transaction.deleteMany({})
+  // a ordem importante, pois se deletarmos as transações primeiro, não conseguiremos deletar os usuários, pois eles estão relacionados a transações
+  await prisma.transaction.deleteMany({})
+  await prisma.user.deleteMany({})
 })
