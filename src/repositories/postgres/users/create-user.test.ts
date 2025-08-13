2e788ffeea353c8a06d434966727450cef41d54e
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-13 13:44:31 -0300

    test: ensure CreateUserRepository calls prisma with correct params

--- src/repositories/postgres/users/create-user.test.ts
+++ src/repositories/postgres/users/create-user.test.ts
@@ -1,13 +1,28 @@
-import { createUserRepositoryResponse } from "@/test"
-import { PostgresCreateUserRepository } from "./create-user"
+import { createUserRepositoryResponse } from '@/test'
+import { PostgresCreateUserRepository } from './create-user'
+import { prisma } from '../../../../prisma/prisma'
 
 describe('CreateUserRepository', () => {
-    it('should create a user on database', async () => {
-        const sut = new PostgresCreateUserRepository()
+    let sut = new PostgresCreateUserRepository()
 
-        const response = await sut.execute(createUserRepositoryResponse)
+    describe('success', () => {
+        it('should create a user on database', async () => {
+            const response = await sut.execute(createUserRepositoryResponse)
 
-        expect(response).not.toBeNull()
-        expect(response).toEqual(createUserRepositoryResponse)
+            expect(response).not.toBeNull()
+            expect(response).toEqual(createUserRepositoryResponse)
+        })
+    })
+
+    describe('validations', () => {
+      it('should call Prisma with correct params', async () => {
+        const prismaSpy = jest.spyOn(prisma.user, 'create')
+
+        await sut.execute(createUserRepositoryResponse)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+          data: createUserRepositoryResponse,
+        })
+      })
     })
 })
