7acc0c3c9c5878d4a6862a7aa74a1b39d8489ea4
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-17 17:44:32 -0300

    refactor: reorganize exports in index.ts for user and transaction controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,12 +1,12 @@
 // users
 export * from './users/create-user'
-export * from './users/get-user-by-id'
-export * from './users/update-user'
 export * from './users/delete-user'
 export * from './users/get-user-balance'
+export * from './users/get-user-by-id'
+export * from './users/update-user'
 
 // transaction
 export * from './transactions/create-transaction'
+export * from './transactions/delete-transaction'
 export * from './transactions/get-transactions-by-user-id'
 export * from './transactions/update-transaction'
-export * from './transactions/delete-transaction'
