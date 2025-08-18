e42ba467c77161ca9de0c06a7e375557195c6ceb
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-18 11:50:13 -0300

    feat: introduce AppError class for standardized error handling across the application

---
+++ src/errors/app-error.ts
@@ -0,0 +1,11 @@
+import { ErrorCode } from './enum'
+
+export abstract class AppError extends Error {
+    public readonly code: ErrorCode
+
+    protected constructor( message: string, code: ErrorCode) {
+        super(message)
+        this.code = code
+        this.name = new.target.name
+    }
+}
