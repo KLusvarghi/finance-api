85ae4d8459c9d587cee484eaa0d6575a2aae6463
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-18 11:48:51 -0300

    refactor: enhance user response helpers to accept custom messages and error codes

--- src/controllers/_helpers/user.ts
+++ src/controllers/_helpers/user.ts
@@ -1,10 +1,17 @@
 import { badRequest, notFound } from './http'
 
-export const userNotFoundResponse = (message?: string) => {
-    if (message) {
-        return notFound(message)
-    }
-    return notFound('User not found')
-}
+import { ErrorCode } from '@/errors'
+import { ResponseMessage } from '@/shared'
 
-export const userBadRequestResponse = () => badRequest('Missing param: userId')
+export const userNotFoundResponse = (
+    message: string,
+    code: ErrorCode = ErrorCode.USER_NOT_FOUND,
+) => notFound(message, code)
+
+export const userIdMissingResponse = () =>
+    badRequest(ResponseMessage.USER_ID_MISSING, ErrorCode.USER_ID_MISSING)
+
+export const emailAlreadyExistsResponse = (
+    message: string,
+    code: ErrorCode = ErrorCode.EMAIL_ALREADY_EXISTS,
+) => badRequest(message, code)
