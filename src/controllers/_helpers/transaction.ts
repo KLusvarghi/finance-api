f9de23b10932b5927103dd2a34df9b0918dab517
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-18 11:48:41 -0300

    refactor: enhance transactionNotFoundResponse to accept custom messages and error codes

--- src/controllers/_helpers/transaction.ts
+++ src/controllers/_helpers/transaction.ts
@@ -1,3 +1,8 @@
 import { notFound } from './http'
 
-export const transactionNotFoundResponse = () => notFound('Transaction not found.')
+import { ErrorCode } from '@/errors'
+
+export const transactionNotFoundResponse = (
+    message: string,
+    code: ErrorCode = ErrorCode.TRANSACTION_NOT_FOUND,
+) => notFound(message, code)
