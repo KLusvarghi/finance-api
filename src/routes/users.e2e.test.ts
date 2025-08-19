3819adf01c5757854134f67981e8ca2e81c4c58a
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-19 12:21:44 -0300

    test: ensure GET /api/users/ returns 404 if user not found

--- src/routes/users.e2e.test.ts
+++ src/routes/users.e2e.test.ts
@@ -3,28 +3,18 @@ import request from 'supertest'
 import { app } from '../app'
 import { ResponseMessage } from '../shared'
 
-import { createUserParams, updateUserParams, userResponse } from '@/test'
+import {
+    createUserParams,
+    updateUserParams,
+    userId,
+    userResponse,
+} from '@/test'
 import { faker } from '@faker-js/faker'
 import { TransactionType } from '@prisma/client'
 
 // Ao invpes de chamar a rota, poderiamos chamar direto o Prisma, mas dessa forma chamando nossas rotas, conseguimos testar o fluxo completo, desde a requisição até a resposta
 
 describe('User Routes E2E Tests', () => {
-    describe('POST /api/users', () => {
-        it('should return 201 when user is created', async () => {
-            const { body: responseBody } = await request(app)
-                .post('/api/users')
-                .send(createUserParams)
-                .expect(201)
-
-            expect(responseBody.data).toEqual({
-                ...userResponse,
-                id: expect.any(String),
-            })
-            expect(responseBody.message).toBe(ResponseMessage.USER_CREATED)
-        })
-    })
-
     describe('GET /api/users/:id', () => {
         it('should return 200 when user is found', async () => {
             const { body: createdUser } = await request(app)
@@ -39,27 +29,15 @@ describe('User Routes E2E Tests', () => {
             expect(responseBody.data).toEqual(createdUser.data)
             expect(responseBody.message).toBe(ResponseMessage.SUCCESS)
         })
-    })
 
-    describe('PATCH /api/users/:userId', () => {
-        it('should return 200 when user is updated', async () => {
-            const { body: createdUser } = await request(app)
-                .post(`/api/users`)
-                .send(createUserParams)
-                .expect(201)
-
+        it('should return 404 when user is not found', async () => {
             const { body: responseBody } = await request(app)
-                .patch(`/api/users/${createdUser.data.id}`)
-                .send(updateUserParams)
-                .expect(200)
+                .get(`/api/users/${userId}`)
+                .expect(404)
 
-            expect(responseBody.data).toEqual({
-                ...createdUser.data,
-                ...updateUserParams,
-                password: undefined,
-            })
-            expect(responseBody.data).not.toHaveProperty('password')
-            expect(responseBody.message).toBe(ResponseMessage.USER_UPDATED)
+            expect(responseBody.message).toBe(
+                `User with id ${userId} not found`,
+            )
         })
     })
 
@@ -113,6 +91,43 @@ describe('User Routes E2E Tests', () => {
                 balance: '6000',
             })
             expect(responseBody.message).toBe(ResponseMessage.SUCCESS)
+        })
+    })
+
+    describe('POST /api/users', () => {
+        it('should return 201 when user is created', async () => {
+            const { body: responseBody } = await request(app)
+                .post('/api/users')
+                .send(createUserParams)
+                .expect(201)
+
+            expect(responseBody.data).toEqual({
+                ...userResponse,
+                id: expect.any(String),
+            })
+            expect(responseBody.message).toBe(ResponseMessage.USER_CREATED)
+        })
+    })
+
+    describe('PATCH /api/users/:userId', () => {
+        it('should return 200 when user is updated', async () => {
+            const { body: createdUser } = await request(app)
+                .post(`/api/users`)
+                .send(createUserParams)
+                .expect(201)
+
+            const { body: responseBody } = await request(app)
+                .patch(`/api/users/${createdUser.data.id}`)
+                .send(updateUserParams)
+                .expect(200)
+
+            expect(responseBody.data).toEqual({
+                ...createdUser.data,
+                ...updateUserParams,
+                password: undefined,
+            })
+            expect(responseBody.data).not.toHaveProperty('password')
+            expect(responseBody.message).toBe(ResponseMessage.USER_UPDATED)
         })
     })
 
