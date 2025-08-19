6465224d70168321260405b8e97b1bec464546da
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-19 10:58:16 -0300

    test: ensure GET /api/users/:userId/balance return 200 when user balance is found

--- src/routes/users.e2e.test.ts
+++ src/routes/users.e2e.test.ts
@@ -5,6 +5,8 @@ import { ResponseMessage } from '../shared'
 
 import { createUserParams, updateUserParams, userResponse } from '@/test'
 
+// Ao invpes de chamar a rota, poderiamos chamar direto o Prisma, mas dessa forma chamando nossas rotas, conseguimos testar o fluxo completo, desde a requisição até a resposta
+
 describe('User Routes E2E Tests', () => {
     describe('POST /api/users', () => {
         it('should return 201 when user is created', async () => {
@@ -56,6 +58,27 @@ describe('User Routes E2E Tests', () => {
             })
             expect(responseBody.data).not.toHaveProperty('password')
             expect(responseBody.message).toBe(ResponseMessage.USER_UPDATED)
+        })
+    })
+
+    describe('GET /api/users/:userId/balance', () => {
+        it('should return 200 when user balance is found', async () => {
+            const { body: createdUser } = await request(app)
+                .post(`/api/users`)
+                .send(createUserParams)
+                .expect(201)
+
+            const { body: responseBody } = await request(app)
+                .get(`/api/users/${createdUser.data.id}/balance`)
+                .expect(200)
+
+            expect(responseBody.data).toEqual({
+                balance: '0',
+                earnings: '0',
+                expenses: '0',
+                investments: '0',
+            })
+            expect(responseBody.message).toBe(ResponseMessage.SUCCESS)
         })
     })
 })
