8b25519157c6d2c3daccbe75e9bb08bac7f55c49
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-18 22:59:56 -0300

    refactor: replace inline user and transaction route handlers with dedicated routers for improved organization

--- index.ts
+++ index.ts
@@ -1,16 +1,6 @@
 import express from 'express'
 
-import {
-    makeCreateTransactionController,
-    makeCreateUserController,
-    makeDeleteTransactionController,
-    makeDeleteUserController,
-    makeGetTransactionsByUserIdController,
-    makeGetUserBalanceController,
-    makeGetUserByIdController,
-    makeUpdateTransactionController,
-    makeUpdateUserController,
-} from '@/factories/controllers'
+import { transactionsRouter, usersRouter } from './src/routes'
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -18,87 +8,9 @@ const port = process.env.PORT || 3001
 // por padrão, o express não suporta json, então temos que especificar aqui que toda requisição que recebe um req que contem um content-type do tipo JSON, ele adapte para receber
 app.use(express.json())
 
-// cria user
-app.post('/api/users', async (request, response) => {
-    const createUserController = makeCreateUserController()
-
-    const { statusCode, body } = await createUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-// consulta user
-app.get('/api/users/:userId', async (request, response) => {
-    const getUserByIdController = makeGetUserByIdController()
-
-    const { statusCode, body } = await getUserByIdController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-// get balance
-app.get('/api/users/:userId/balance', async (request, response) => {
-    const getUserBalanceController = makeGetUserBalanceController()
-
-    const { statusCode, body } = await getUserBalanceController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-// udpate user
-app.patch('/api/users/:userId', async (request, response) => {
-    const updateUserController = makeUpdateUserController()
-
-    const { statusCode, body } = await updateUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-// deletar user
-app.delete('/api/users/:userId', async (request, response) => {
-    const deleteUserController = makeDeleteUserController()
-
-    const { statusCode, body } = await deleteUserController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.post('/api/transactions', async (request, response) => {
-    const createTransactionController = makeCreateTransactionController()
-
-    const { statusCode, body } =
-        await createTransactionController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.get('/api/transactions', async (request, response) => {
-    const getTransactionsByUserIdController =
-        makeGetTransactionsByUserIdController()
-
-    const { statusCode, body } =
-        await getTransactionsByUserIdController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-    const updateTransactionsController = makeUpdateTransactionController()
-
-    const { statusCode, body } =
-        await updateTransactionsController.execute(request)
-
-    response.status(statusCode).send(body)
-})
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-    const deleteTransactionsController = makeDeleteTransactionController()
-
-    const { statusCode, body } =
-        await deleteTransactionsController.execute(request)
-
-    response.status(statusCode).send(body)
-})
+// toda vez que uma requisição for feita para a rota /api/users, o express vai usar o usersRouter para lidar com a requisição
+app.use('/api/users', usersRouter)
+app.use('/api/transactions', transactionsRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
