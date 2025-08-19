1751201c1b77cca7a9be558461a9286543760231
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-19 10:38:19 -0300

    test: ensure GET /api/users/ should return 200 when user is found

--- src/routes/users.e2e.test.ts
+++ src/routes/users.e2e.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest'
 
-import { app } from '../..'
+import { app } from '../app'
 import { ResponseMessage } from '../shared'
 
 import { createUserParams, userResponse } from '@/test'
@@ -8,17 +8,32 @@ import { createUserParams, userResponse } from '@/test'
 describe('User Routes E2E Tests', () => {
     describe('POST /api/users', () => {
         it('should return 201 when user is created', async () => {
-            const response = await request(app)
+            const { body: responseBody } = await request(app)
                 .post('/api/users')
                 .send(createUserParams)
                 .expect(201)
                 .expect('Content-Type', /json/)
 
-            expect(response.body.data).toEqual({
+            expect(responseBody.data).toEqual({
                 ...userResponse,
                 id: expect.any(String),
             })
-            expect(response.body.message).toBe(ResponseMessage.USER_CREATED)
+            expect(responseBody.message).toBe(ResponseMessage.USER_CREATED)
+        })
+    })
+
+    describe('GET /api/users/:id', () => {
+        it('should return 200 when user is found', async () => {
+            const { body: createdUser } = await request(app)
+                .post(`/api/users`)
+                .send(createUserParams)
+                .expect(201)
+
+            const { body: responseBody } = await request(app).get(
+                `/api/users/${createdUser.data.id}`,
+            )
+
+            expect(responseBody.data).toEqual(createdUser.data)
         })
     })
 })
