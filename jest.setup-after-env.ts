58f23a42da4347cf7c7bdfab4258fbc01c02ad26
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-26 12:50:53 -0300

    chore: improve comments in jest.setup-after-env.ts for clarity and maintainability

--- jest.setup-after-env.ts
+++ jest.setup-after-env.ts
@@ -2,8 +2,8 @@ import { prisma } from './prisma/prisma'
 
 // antes de cada teste, deletamos todos os usuários e transações, afim de nenhum dado fique no banco e interfira nos testes
 beforeEach(async () => {
-  // limpa as tabelas do nosso banco de dados antes de cada teste
-  // a ordem importante, pois se deletarmos as transações primeiro, não conseguiremos deletar os usuários, pois eles estão relacionados a transações
-  await prisma.transaction.deleteMany({})
-  await prisma.user.deleteMany({})
+    // limpa as tabelas do nosso banco de dados antes de cada teste
+    // a ordem importante, pois se deletarmos as transações primeiro, não conseguiremos deletar os usuários, pois eles estão relacionados a transações
+    await prisma.transaction.deleteMany({})
+    await prisma.user.deleteMany({})
 })
