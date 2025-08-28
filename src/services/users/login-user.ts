7b7a23fee6fa50d4136ef0141604359dee56aaba
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-28 12:16:10 -0300

    feat: implement LoginUserService for user authentication with email and password validation

---
+++ src/services/users/login-user.ts
@@ -0,0 +1,48 @@
+import { PasswordComparatorAdapter, TokenGeneratorAdapter } from '@/adapters'
+import { InvalidPasswordError, UserNotFoundError } from '@/errors'
+import {
+    GetUserByEmailRepository,
+    TokenGeneratorAdapterResponse,
+    UserRepositoryResponse,
+} from '@/shared'
+
+export class LoginUserService {
+    constructor(
+        private getUserByEmailRepository: GetUserByEmailRepository,
+        private passwordComparator: PasswordComparatorAdapter,
+        private tokenGeneratorAdapter: TokenGeneratorAdapter,
+    ) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.passwordComparator = passwordComparator
+        this.tokenGeneratorAdapter = tokenGeneratorAdapter
+    }
+    async execute(
+        email: string,
+        password: string,
+    ): Promise<
+        UserRepositoryResponse & { tokens: TokenGeneratorAdapterResponse }
+    > {
+        // verificar se o e-mail válido (se não houver usuério com esse e-amil lançamos uma exceção)
+        const user = await this.getUserByEmailRepository.execute(email)
+        if (!user) {
+            throw new UserNotFoundError(email)
+        }
+
+        // verificar se a senha é válida (se não estiver lançamos uma exceção)
+        const isPasswordValid = await this.passwordComparator.execute(
+            password,
+            user.password,
+        )
+
+        if (!isPasswordValid) {
+            throw new InvalidPasswordError()
+        }
+
+        // retornar o token JWT
+        return {
+            ...user,
+            // gerar os tokens JWT
+            tokens: await this.tokenGeneratorAdapter.execute(user.id),
+        }
+    }
+}
