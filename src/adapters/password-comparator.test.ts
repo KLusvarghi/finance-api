c6190e70f94716d62edf68257f37108def01a23c
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-28 12:24:28 -0300

    test: enhance PasswordComparatorAdapter tests to verify password matching and non-matching scenarios

--- src/adapters/password-comparator.test.ts
+++ src/adapters/password-comparator.test.ts
@@ -1,18 +1,29 @@
+import bcrypt from 'bcrypt'
+
 import { PasswordComparatorAdapter } from './password-comparator'
 
-import { faker } from '@faker-js/faker'
+// Faz mock de todo o módulo bcrypt
+jest.mock('bcrypt')
 
 describe('PasswordComparatorAdapter', () => {
-    it('should return true if the password is valid', async () => {
-        // arrange
-        const sut = new PasswordComparatorAdapter()
-        const password = faker.internet.password()
-        const hashedPassword = password
+    const sut = new PasswordComparatorAdapter()
+    const password = 'plain_password'
+    const hashedPassword = '$2b$12$saltsaltsaltsaltsaltHash'
 
-        // act
-        const response = await sut.execute(password, hashedPassword)
+    it('should return true when passwords match', async () => {
+        ;(bcrypt.compare as jest.Mock).mockResolvedValue(true)
 
-        // assert
-        expect(response).toBe(true)
+        const result = await sut.execute(password, hashedPassword)
+
+        expect(bcrypt.compare).toHaveBeenCalledWith(password, hashedPassword)
+        expect(result).toBe(true)
+    })
+
+    it('should return false when passwords do not match', async () => {
+        ;(bcrypt.compare as jest.Mock).mockResolvedValue(false)
+
+        const result = await sut.execute(password, hashedPassword)
+
+        expect(result).toBe(false)
     })
 })
