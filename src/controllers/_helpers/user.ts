6f494174dbb1678f601653c522f5ad213c69cfc0
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-29 09:28:57 -0300

    feat: add invalid password response handling in user helper functions

--- src/controllers/_helpers/user.ts
+++ src/controllers/_helpers/user.ts
@@ -1,4 +1,4 @@
-import { badRequest, notFound } from './http'
+import { badRequest, notFound, unauthorized } from './http'
 
 import { ErrorCode } from '@/errors'
 import { ResponseMessage } from '@/shared'
@@ -15,3 +15,6 @@ export const emailAlreadyExistsResponse = (
     message: string,
     code: ErrorCode = ErrorCode.EMAIL_ALREADY_EXISTS,
 ) => badRequest(message, code)
+
+export const invalidPasswordResponse = () =>
+    unauthorized(ErrorCode.INVALID_PASSWORD)
