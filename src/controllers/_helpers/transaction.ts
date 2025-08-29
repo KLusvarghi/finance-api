bc17df155192972082975ebec8fc618ca75f4a65
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-29 18:00:37 -0300

    refactor: update unauthorized response message to use a constant for improved consistency and clarity

--- src/controllers/_helpers/transaction.ts
+++
@@ -1,8 +0,0 @@
-import { notFound } from './http'
-
-import { ErrorCode } from '@/errors'
-
-export const transactionNotFoundResponse = (
-    message: string,
-    code: ErrorCode = ErrorCode.TRANSACTION_NOT_FOUND,
-) => notFound(message, code)
