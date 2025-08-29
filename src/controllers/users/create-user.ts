ef016bc1ad9e4c21bce0effa0b17b00d81c2f716
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-29 19:02:52 -0300

    refactor: update CreateUserController to include token generation in response type for enhanced user creation

--- src/controllers/users/create-user.ts
+++ src/controllers/users/create-user.ts
@@ -16,11 +16,16 @@ import {
     HttpRequest,
     HttpResponse,
     ResponseMessage,
+    TokenGeneratorAdapterResponse,
     UserPublicResponse,
 } from '@/shared'
 
 export class CreateUserController
-    implements Controller<CreateUserParams, UserPublicResponse>
+    implements
+        Controller<
+            CreateUserParams,
+            UserPublicResponse & { tokens: TokenGeneratorAdapterResponse }
+        >
 {
     private createUserService: CreateUserService
 
@@ -30,7 +35,11 @@ export class CreateUserController
 
     async execute(
         httpRequest: HttpRequest,
-    ): Promise<HttpResponse<UserPublicResponse>> {
+    ): Promise<
+        HttpResponse<
+            UserPublicResponse & { tokens: TokenGeneratorAdapterResponse }
+        >
+    > {
         try {
             // validar a requisição (campos obrigatório, email e tamenho de senha)
             const params = httpRequest.body
@@ -44,7 +53,12 @@ export class CreateUserController
             )
 
             // retornar a resposta para o user (status code)
-            return created(createdUser, ResponseMessage.USER_CREATED)
+            return created(
+                createdUser,
+                ResponseMessage.USER_CREATED,
+            ) as HttpResponse<
+                UserPublicResponse & { tokens: TokenGeneratorAdapterResponse }
+            >
         } catch (error) {
             if (error instanceof ZodError) {
                 return handleZodValidationError(error)
