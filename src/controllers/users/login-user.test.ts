14c58d7acd8c2a8a4f0dc434a9a508c33a1b9c86
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-28 21:24:29 -0300

    test: implement unit tests for LoginUserController to validate successful login response

---
+++ src/controllers/users/login-user.test.ts
@@ -0,0 +1,201 @@
+import { LoginUserController } from './login-user'
+
+import { InvalidPasswordError, UserNotFoundError } from '@/errors'
+import {
+    ResponseMessage,
+    ResponseZodMessages,
+    TokenGeneratorAdapterResponse,
+    UserRepositoryResponse,
+} from '@/shared'
+import {
+    createUserRepositoryResponse,
+    createUserServiceResponse,
+    loginUserHttpRequest as baseHttpRequest,
+    tokenGeneratorAdapterResponse,
+    userId,
+} from '@/test'
+import { faker } from '@faker-js/faker'
+
+describe('LoginUserController', () => {
+    let sut: LoginUserController
+    let loginUserService: LoginUserServiceStub
+
+    class LoginUserServiceStub {
+        async execute(
+            _email: string,
+            _password: string,
+        ): Promise<
+            UserRepositoryResponse & { tokens: TokenGeneratorAdapterResponse }
+        > {
+            return Promise.resolve({
+                ...createUserServiceResponse,
+                password: createUserRepositoryResponse.password,
+                tokens: tokenGeneratorAdapterResponse,
+            })
+        }
+    }
+
+    const makeSut = () => {
+        const loginUserService = new LoginUserServiceStub()
+        const sut = new LoginUserController(loginUserService)
+
+        return {
+            loginUserService,
+            sut,
+        }
+    }
+
+    beforeEach(() => {
+        const { sut: controller, loginUserService: service } = makeSut()
+        sut = controller
+        loginUserService = service
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+        jest.restoreAllMocks()
+    })
+
+    describe('error handling', () => {
+        it('should return 404 if user is not found', async () => {
+            jest.spyOn(loginUserService, 'execute').mockImplementationOnce(
+                async () => {
+                    throw new UserNotFoundError(userId)
+                },
+            )
+
+            const response = await sut.execute(baseHttpRequest)
+
+            expect(response.statusCode).toBe(404)
+            expect(response.body?.message).toBe(
+                `User with id ${userId} not found`,
+            )
+        })
+
+        it('should return 401 and throws InvalidPasswordError if password is invalid', async () => {
+            jest.spyOn(loginUserService, 'execute').mockImplementationOnce(
+                async () => {
+                    throw new InvalidPasswordError()
+                },
+            )
+
+            const response = await sut.execute(baseHttpRequest)
+
+            expect(response.statusCode).toBe(401)
+            expect(response.body?.message).toBe(ResponseMessage.UNAUTHORIZED)
+        })
+    })
+
+    describe('success', () => {
+        it('should return 200 and the user when login is successful', async () => {
+            const { sut } = makeSut()
+
+            const response = await sut.execute(baseHttpRequest)
+
+            expect(response.statusCode).toBe(200)
+            expect(response.body?.data).toEqual({
+                ...createUserServiceResponse,
+                password: createUserRepositoryResponse.password,
+                tokens: tokenGeneratorAdapterResponse,
+            })
+            expect(response.body?.message).toBe(
+                ResponseMessage.USER_LOGIN_SUCCESS,
+            )
+        })
+    })
+
+    describe('validations', () => {
+        describe('email', () => {
+            it('should return 400 and throws ZodError if email is invalid', async () => {
+                const response = await sut.execute({
+                    ...baseHttpRequest,
+                    body: {
+                        ...baseHttpRequest.body,
+                        email: 'invalid_email',
+                    },
+                })
+
+                expect(response.statusCode).toBe(400)
+                expect(response.body?.message).toBe(
+                    ResponseZodMessages.email.invalid,
+                )
+            })
+            it('should return 400 and throws ZodError if email is empty', async () => {
+                const response = await sut.execute({
+                    ...baseHttpRequest,
+                    body: {
+                        ...baseHttpRequest.body,
+                        email: '',
+                    },
+                })
+
+                expect(response.statusCode).toBe(400)
+                expect(response.body?.message).toBe(
+                    ResponseZodMessages.email.invalid,
+                )
+            })
+
+            it('should return 400 and throws ZodError if email is not provided', async () => {
+                const response = await sut.execute({
+                    ...baseHttpRequest,
+                    body: {
+                        ...baseHttpRequest.body,
+                        email: undefined,
+                    },
+                })
+
+                expect(response.statusCode).toBe(400)
+                expect(response.body?.message).toBe(
+                    ResponseZodMessages.email.invalid,
+                )
+            })
+        })
+
+        describe('password', () => {
+            it('should return 400 and throws ZodError if password is invalid', async () => {
+                const response = await sut.execute({
+                    ...baseHttpRequest,
+                    body: {
+                        ...baseHttpRequest.body,
+                        password: faker.internet.password({ length: 5 }),
+                    },
+                })
+
+                expect(response.statusCode).toBe(400)
+                expect(response.body?.message).toBe(
+                    ResponseZodMessages.password.minLength,
+                )
+            })
+
+            it('should return 400 and throws ZodError if password is empty', async () => {
+                const response = await sut.execute({
+                    ...baseHttpRequest,
+                    body: {
+                        ...baseHttpRequest.body,
+                        password: '',
+                    },
+                })
+
+                expect(response.statusCode).toBe(400)
+                expect(response.body?.message).toBe(
+                    ResponseZodMessages.password.minLength,
+                )
+            })
+
+            it('should return 400 and throws ZodError if password is not provided', async () => {
+                const response = await sut.execute({
+                    ...baseHttpRequest,
+                    body: {
+                        ...baseHttpRequest.body,
+                        password: undefined,
+                    },
+                })
+
+                expect(response.statusCode).toBe(400)
+                expect(response.body?.message).toBe(
+                    ResponseZodMessages.password.required,
+                )
+            })
+        })
+    })
+})
