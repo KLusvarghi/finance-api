3f0959bdaffb6efad6958fd63572de204d062eb7
KLusvarghi <kauaolusvarghi@gmail.com>
2025-08-29 18:02:13 -0300

    feat: add authentication middleware to user routes for enhanced security

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,10 +8,11 @@ import {
     makeLoginUserController,
     makeUpdateUserController,
 } from '@/factories/controllers/'
+import { auth } from '@/middlewares/auth'
 
 export const usersRouter = Router()
 
-usersRouter.get('/:userId', async (request, response) => {
+usersRouter.get('/:userId', auth, async (request, response) => {
     const getUserByIdController = makeGetUserByIdController()
 
     const { statusCode, body } = await getUserByIdController.execute(request)
@@ -19,7 +20,7 @@ usersRouter.get('/:userId', async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-usersRouter.get('/:userId/balance', async (request, response) => {
+usersRouter.get('/:userId/balance', auth, async (request, response) => {
     const getUserBalanceController = makeGetUserBalanceController()
 
     const { statusCode, body } = await getUserBalanceController.execute(request)
@@ -35,7 +36,7 @@ usersRouter.post('/', async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-usersRouter.patch('/:userId', async (request, response) => {
+usersRouter.patch('/:userId', auth, async (request, response) => {
     const updateUserController = makeUpdateUserController()
 
     const { statusCode, body } = await updateUserController.execute(request)
@@ -43,7 +44,7 @@ usersRouter.patch('/:userId', async (request, response) => {
     response.status(statusCode).send(body)
 })
 
-usersRouter.delete('/:userId', async (request, response) => {
+usersRouter.delete('/:userId', auth, async (request, response) => {
     const deleteUserController = makeDeleteUserController()
 
     const { statusCode, body } = await deleteUserController.execute(request)
