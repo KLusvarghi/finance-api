0468ad32728ee5bf1c41244f36dfaf3ef6b82efd
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-08 18:28:00 -0300

    fix: enhance GetTransactionsByUserIdController to validate date range parameters and handle Zod validation errors

--- src/controllers/transactions/get-transactions-by-user-id.ts
+++ src/controllers/transactions/get-transactions-by-user-id.ts
@@ -1,6 +1,14 @@
-import { notFoundResponse, ok, serverError } from '../_helpers'
+import { ZodError } from 'zod'
+
+import {
+    handleZodValidationError,
+    notFoundResponse,
+    ok,
+    serverError,
+} from '../_helpers'
 
 import { UserNotFoundError } from '@/errors'
+import { getTransactionsByUserIdSchema } from '@/schemas'
 import {
     GetTransactionsByUserIdRequest,
     GetTransactionsByUserIdService,
@@ -24,16 +32,29 @@ export class GetTransactionsByUserIdController
         try {
             // para que possamos pegar um valor que é passsado por uma query na url e não como um parametro no body, fazemos assim:
             const { userId } = httpRequest.headers
+            const { from, to } = httpRequest.query
+
+            await getTransactionsByUserIdSchema.parseAsync({ userId, from, to })
 
             const transactions =
-                await this.getTransactionByUserIdService.execute(userId)
+                await this.getTransactionByUserIdService.execute(
+                    userId,
+                    from,
+                    to,
+                )
 
             return ok(transactions)
         } catch (error) {
             console.error(error)
+
+            if (error instanceof ZodError) {
+                return handleZodValidationError(error)
+            }
+
             if (error instanceof UserNotFoundError) {
                 return notFoundResponse(error)
             }
+
             return serverError()
         }
     }
