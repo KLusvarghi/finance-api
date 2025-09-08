5edbfe3d22ef10343eba710de38991b2484605e6
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-08 20:38:34 -0300

    refactor: update GetUserBalanceService to implement ServiceWithMultipleParams and include date range parameters for balance retrieval

--- src/services/users/get-user-balance.ts
+++ src/services/users/get-user-balance.ts
@@ -2,12 +2,18 @@ import { UserNotFoundError } from '@/errors'
 import {
     GetUserBalanceRepository,
     GetUserByIdRepository,
-    SimpleService,
+    ServiceWithMultipleParams,
     UserBalanceRepositoryResponse,
 } from '@/shared'
 
 export class GetUserBalanceService
-    implements SimpleService<string, UserBalanceRepositoryResponse>
+    implements
+        ServiceWithMultipleParams<
+            string,
+            string,
+            string,
+            UserBalanceRepositoryResponse
+        >
 {
     private getUserByIdRepository: GetUserByIdRepository
     private getUserBalanceRepository: GetUserBalanceRepository
@@ -20,14 +26,22 @@ export class GetUserBalanceService
         this.getUserBalanceRepository = getUserBalanceRepository
     }
 
-    async execute(userId: string): Promise<UserBalanceRepositoryResponse> {
+    async execute(
+        userId: string,
+        from: string,
+        to: string,
+    ): Promise<UserBalanceRepositoryResponse> {
         const user = await this.getUserByIdRepository.execute(userId)
 
         if (!user) {
             throw new UserNotFoundError(userId)
         }
 
-        const userBalance = await this.getUserBalanceRepository.execute(userId)
+        const userBalance = await this.getUserBalanceRepository.execute(
+            userId,
+            from,
+            to,
+        )
 
         return userBalance
     }
