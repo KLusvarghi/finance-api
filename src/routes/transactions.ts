a7cba070b656fac9076b3564787015336c96ad5b
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-16 19:58:47 -0300

    refactor: remove unused response handling for missing transaction IDs in transactions routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,4 +1,4 @@
-import { Response, Router } from 'express'
+import { Router } from 'express'
 
 import { adaptRoute } from './adapters/express-route-adapter'
 
@@ -16,7 +16,6 @@ import {
     getTransactionsByUserIdSchema,
     updateTransactionSchema,
 } from '@/schemas'
-import { ResponseMessage } from '@/shared'
 
 export const transactionsRouter = Router()
 
@@ -36,16 +35,6 @@ transactionsRouter.post(
     adaptRoute(makeCreateTransactionController()),
 )
 
-// TODO: ver se é necessário
-// Rota para lidar com transactionId ausente - deve vir ANTES da rota com parâmetro
-transactionsRouter.patch('/me/', auth, (res: Response) => {
-    res.status(400).json({
-        success: false,
-        message: ResponseMessage.TRANSACTION_ID_MISSING,
-        code: 'TRANSACTION_ID_MISSING',
-    })
-})
-
 // Rota para atualizar uma transação
 transactionsRouter.patch(
     '/me/:transactionId',
@@ -53,16 +42,6 @@ transactionsRouter.patch(
     validate(updateTransactionSchema),
     adaptRoute(makeUpdateTransactionController()),
 )
-
-// TODO: ver se é necessário
-// Rota para lidar com transactionId ausente no DELETE - deve vir ANTES da rota com parâmetro
-transactionsRouter.delete('/me/', auth, (res: Response) => {
-    res.status(400).json({
-        success: false,
-        message: ResponseMessage.TRANSACTION_ID_MISSING,
-        code: 'TRANSACTION_ID_MISSING',
-    })
-})
 
 // Rota para deletar uma transação
 transactionsRouter.delete(
