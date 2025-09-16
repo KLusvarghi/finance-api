f2a84971f51edc4cd042db087fa841bd58177f4b
KLusvarghi <kauaolusvarghi@gmail.com>
2025-09-16 15:22:36 -0300

    refactor: enhance token verification logic to handle expired and invalid tokens with custom error handling

--- src/adapters/token-veriry.ts
+++ src/adapters/token-veriry.ts
@@ -1,10 +1,23 @@
-import jwt from 'jsonwebtoken'
+import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken'
+
+import { ExpiredTokenError, InvalidTokenError } from '@/errors'
 
 export class TokenVerifierAdapter {
     async execute(
         token: string,
         secret: string,
     ): Promise<string | jwt.JwtPayload> {
-        return jwt.verify(token, secret)
+        try {
+            return jwt.verify(token, secret)
+        } catch (error) {
+            console.error('Token verification failed:', error)
+            if (error instanceof TokenExpiredError) {
+                throw new ExpiredTokenError()
+            }
+            if (error instanceof JsonWebTokenError) {
+                throw new InvalidTokenError()
+            }
+            throw new InvalidTokenError()
+        }
     }
 }
