a2a882cb9364447f672f3d1efd7b63c534df2b36
KLusvarghi <kauaolusvarghi@gmail.com>
2025-10-29 17:15:41 -0300

    chore: refactor ESLint configuration for Node to improve export structure and clarify comments

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,20 +1,25 @@
 import baseConfig from './base.js';
-import tseslint from 'typescript-eslint';
+// Not needed here – tseslint is only required in base.js to provide the parser and recommended configs for all Node targets.
 import jestPlugin from 'eslint-plugin-jest';
 import globals from 'globals';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default tseslint.config(...baseConfig, {
-  files: ['**/*.test.ts', '**/*.spec.ts'],
-  plugins: {
-    jest: jestPlugin,
-  },
-  languageOptions: {
-    globals: {
-      ...globals.jest,
+// export default tseslint.config(...baseConfig, {
+export default [
+  ...baseConfig,
+  {
+    // Objeto de ignores específico para a API
+    files: ['**/*.test.ts', '**/*.spec.ts'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
     },
   },
-  rules: {
-    ...jestPlugin.configs.recommended.rules,
-  },
-}); 
+];
